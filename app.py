03514f2bdaf0d7e490ab98d5a160d0bd68290b03
elis-elis <elisprostotak@gmail.com>
2024-09-19 13:52:53 +0200

    add app.errorhandler function, in app.py, and add logging.info to app.routes

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from flask import Flask, jsonify
 from services.iss_service import *
 from config.config import Config
 
-
-load_dotenv()   # This will load the variables from the .env file into the environment
+load_dotenv()  # This will load the variables from the .env file into the environment
 app = Flask(__name__)
 app.config.from_object(Config)
-
 
 logging.basicConfig(
     level=getattr(logging, Config.LOG_LEVEL),
@@ -21,14 +19,22 @@ logging.info("Starting Flask ISS Tracking App")
 
 @app.route('/api/iss-location', methods=['GET'])
 def iss_location():
+    logging.info("Received request for ISS location")
     location = get_iss_location()
     return jsonify(location)
 
 
 @app.route('/api/iss-pass/<float:lat>/<float:lon>', methods=['GET'])
 def iss_pass(lat, lon):
+    logging.info(f"Received request for ISS pass times for lat: {lat}, lon: {lon}")
     pass_times = get_iss_pass_times(lat, lon)
     return jsonify(pass_times)
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    logging.error(f"Unhandled exception: {e}")
+    return jsonify({'error': 'An internal server error happened'}), 500
 
 
 if __name__ == '__main__':
